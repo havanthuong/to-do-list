4223010e73c1a9347e42a4aff87942d8c5ebae11
havanthuong <havanthuong2000@gmail.com>
2022-04-27 14:28:20 +0700

    todo v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  //get data from local storage - string and convert data to array
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'));
+    return storageJobs ?? [];
+  });//
+  const [inputJob, setInputJob] = useState('');
+
+  const handleClick = () => {
+    setJobs(pre => {
+      const newJobs = [...pre, inputJob];
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem('jobs', jsonJobs);
+      return newJobs;
+    });
+    setInputJob('');
+  };
+  const handleDelete = (key) => {
+    jobs.splice(key, 1);
+    setJobs((prev) => {
+      console.log(prev);
+      const jsonJobs = JSON.stringify(prev);
+      localStorage.setItem('jobs', jsonJobs);
+      const storageJobs = JSON.parse(localStorage.getItem('jobs'));
+      return storageJobs;
+    });
+  }
+  const reset = () => {
+    localStorage.removeItem('jobs');
+    setJobs([]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={inputJob} onChange={e => setInputJob(e.target.value)} />
+      <button type="submit" onClick={handleClick}>Add</button>
+      <div className="">
+        <ul>
+          {jobs.map((value, index) => (
+            <li key={index}>{value}<button onClick={() => handleDelete(index)}>X</button></li>
+          ))}
+        </ul>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
